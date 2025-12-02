4ae80080ed0667cbd0451e88f3e5c8fe3430e20e
Jakub Kiermasz <kiermasz.k@gmail.com>
2025-12-02 22:26:44 +0100

    Initial Commit

---
+++ TokenBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TokenBar
+//
+//  Created by Jakub Kiermasz on 02/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TokenBar/TokenBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TokenBarApp.swift
+//  TokenBar
+//
+//  Created by Jakub Kiermasz on 02/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct TokenBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
